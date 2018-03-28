01c952641041bfaa600d3c562941264d062451c2
vaibhav.ramdasi <vaibhav.ramdasi@wolterskluwer.com>
2018-03-28 19:56:38 +0530

    and some more

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -3,13 +3,18 @@ import { Http, Response } from '@angular/http';
 import 'rxjs/add/operator/map'
 import { Album, AlbumInfo } from './album';
 import { Observable } from 'rxjs/Observable';
-
+import {Product} from './product';
 @Injectable()
 export class ProductService {
   private _albumUrl: string = '../assets/album.json';
+  private _productsUrl: string = '../assets/products.json';
   constructor(private _http: Http) { }
 
-  getAlbum(id: number):Observable<AlbumInfo> {
-    return this._http.get(this._albumUrl).map(response=><AlbumInfo>response.json());
+  getAlbum(id: number): Observable<AlbumInfo> {
+    return this._http.get(this._albumUrl).map(response => <AlbumInfo>response.json());
+  }
+
+  getProducts():Observable<Product[]>{
+    return this._http.get(this._productsUrl).map(response => <Product[]>response.json());
   }
 }
